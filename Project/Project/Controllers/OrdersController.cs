ba884f1fb9cb8f02811be287502de7151e7afdda
vypq <42968820+vypq@users.noreply.github.com>
2018-09-20 19:08:33 +0700

    Update OrderController

--- Project/Project/Controllers/OrdersController.cs
+++ Project/Project/Controllers/OrdersController.cs
@@ -8,29 +8,22 @@ using System.Web.Mvc;
 
 namespace Project.Controllers
 {
-    
+    [Authorize]
     public class OrdersController : Controller
     {
         MyDatabaseEntities db = new MyDatabaseEntities();
-        
-        [Authorize]
+       
         public ActionResult Index(string searchString)
         {
-<<<<<<< HEAD
             var OrderAndCustomerList = from m in db.Customers select m;
             if (!String.IsNullOrEmpty(searchString))
-=======
             List<Customer> OrderAndCustomerList = db.Customers.ToList();
             return View(OrderAndCustomerList);
-
         }
-
-       
         public ActionResult SaveOrder(string name, String address, Order[] order)
         {
             string result = "Error! Order Is Not Complete!";
             if (name != null && address != null && order != null)
->>>>>>> dmtuan
             {
                 OrderAndCustomerList = OrderAndCustomerList.Where(s => s.Name.Contains(searchString));
             }
