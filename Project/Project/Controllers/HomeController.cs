4293db63ba65167ec7fc20d14a030ed87535a1df
vypq <vypqd00209@fpt.edu.vn>
2018-09-22 18:13:29 +0700

    Commit to master

--- Project/Project/Controllers/HomeController.cs
+++ Project/Project/Controllers/HomeController.cs
@@ -10,21 +10,59 @@ namespace Project.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            MyDatabaseEntities entities = new MyDatabaseEntities();
+            List<Customer> customers = entities.Customers.ToList();
+
+            //Add a Dummy Row.
+            customers.Insert(0, new Customer());
+            return View(customers);
         }
 
-        public ActionResult About()
+        [HttpPost]
+        public JsonResult InsertCustomer(Customer customer)
         {
-            ViewBag.Message = "Your application description page.";
+            using (MyDatabaseEntities entities = new MyDatabaseEntities())
+            {
+                entities.Customers.Add(customer);
+                entities.SaveChanges();
+            }
 
-            return View();
+            return Json(customer);
         }
 
-        public ActionResult Contact()
+        [HttpPost]
+        public ActionResult DeleteCustomer(int customerId)
         {
-            ViewBag.Message = "Your contact page.";
+            using (MyDatabaseEntities entities = new MyDatabaseEntities())
+            {
+                Customer customer = (from c in entities.Customers
+                                     where c.CustomerId == customerId
+                                     select c).FirstOrDefault();
+                entities.Customers.Remove(customer);
+                entities.SaveChanges();
+            }
 
-            return View();
+            //Response.Redirect
+
+            return new EmptyResult();
+        }
+
+        [HttpPost]
+        public ActionResult UpdateCustomer(Customer customer)
+        {
+            using (MyDatabaseEntities entities = new MyDatabaseEntities())
+            {
+                Customer updatedCustomer = (from c in entities.Customers
+                                            where c.CustomerId == customer.CustomerId
+                                            select c).FirstOrDefault();
+                updatedCustomer.Name = customer.Name;
+                updatedCustomer.Address = customer.Address;
+                updatedCustomer.OrderDate = customer.OrderDate;
+                updatedCustomer.Phone = customer.Phone;
+                entities.SaveChanges();
+            }
+
+            return new EmptyResult();
         }
     }
 }
